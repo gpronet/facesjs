6f4e3b9a0af5cd01c062ba50921eedca2ce97571
gpronet <17197505+gpronet@users.noreply.github.com>
2025-02-27 13:12:47 +0200

    test

--- src/display.ts
+++ src/display.ts
@@ -228,9 +228,9 @@ const drawFeature = (
           }
           console.log((gElement.children[idx2] as HTMLElement).style);
           if (
-            (gElement.children[idx2] as HTMLElement).style.fill ===
+            (gElement.children[idx2] as HTMLElement).style.fill ==
               'url("#' + tmpId + '")' ||
-            (gElement.children[idx2] as HTMLElement).style.fill ===
+            (gElement.children[idx2] as HTMLElement).style.fill ==
               "url(#" + tmpId + ")"
           ) {
             gElement.children[idx2].setAttribute(
