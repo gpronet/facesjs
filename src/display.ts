9a0618b3beda012a3c06d2a8b0e58ad85374770a
gpronet <17197505+gpronet@users.noreply.github.com>
2025-02-27 10:54:53 +0200

    debug

--- src/display.ts
+++ src/display.ts
@@ -301,7 +301,7 @@ const drawFeature = (
     // @ts-expect-error
     scaleCentered(svg.lastChild, fatScale(face.fatness), 1);
   }
-  console.log(svg);
+  console.log(svg.lastChild);
 };
 
 export const display = (
