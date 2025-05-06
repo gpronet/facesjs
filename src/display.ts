e9fb2f8fb0f28a01b4c7f27805dfda2c496831a5
gpronet <17197505+gpronet@users.noreply.github.com>
2025-05-06 15:37:19 +0300

    test

--- src/display.ts
+++ src/display.ts
@@ -117,7 +117,7 @@ const drawFeature = (
     return;
   }
   if (
-    ["hat", "hat2", "hat3", "cap1", "cap2"].includes(face.accessories.id) &&
+    ["hat", "hat2", "hat3"].includes(face.accessories.id) &&
     info.name == "hair"
   ) {
     if (
